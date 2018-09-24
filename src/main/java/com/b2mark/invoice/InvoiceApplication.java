d134e5d54ed60c644f08b709c1254cac15817482
Administrator <alkhandani@gmail.com>
2018-09-24 14:51:07 +0330

    solve bug not run refs #336

--- src/main/java/com/b2mark/invoice/InvoiceApplication.java
+++ src/main/java/com/b2mark/invoice/InvoiceApplication.java
@@ -4,9 +4,6 @@ import com.b2mark.invoice.config.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class InvoiceApplication {
