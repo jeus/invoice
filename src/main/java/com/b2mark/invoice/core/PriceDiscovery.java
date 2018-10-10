db8f8235ef9d317f5e9285c636d1de05b5944d83
Administrator <alkhandani@gmail.com>
2018-10-10 14:51:37 +0330

    service discovery for call price discovery finish #570

--- src/main/java/com/b2mark/invoice/core/PriceDiscovery.java
+++ src/main/java/com/b2mark/invoice/core/PriceDiscovery.java
@@ -11,29 +11,49 @@ package com.b2mark.invoice.core;
 
 import com.b2mark.invoice.common.enums.Coin;
 import com.b2mark.invoice.entity.price.Price;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PriceDiscovery {
-    private static final String lastPriceDiscovery = "http://localhost:8080/price/";
+
+    @Qualifier("eurekaClient")
+    @Autowired
+    private EurekaClient eurekaClient;
     private final RestTemplate restTemplate;
 
     public PriceDiscovery(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public Price getPrice(Coin payer,Coin merchant,String driver) {
+    public Price getPrice(Coin payer, Coin merchant, String driver) {
+        Application application = eurekaClient.getApplication("PRICOVERY");
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        String host = instanceInfo.getHostName();
+        int port = instanceInfo.getPort();
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(lastPriceDiscovery).append(payer.getSymbol()).append(merchant.getSymbol());
+        stringBuilder.append(getUrl(host,port)).append(payer.getSymbol().toLowerCase()).append(merchant.getSymbol().toLowerCase());
         Price price = null;
         try {
-            price = restTemplate.getForObject(stringBuilder.toString().toLowerCase(), Price.class);
+            price = restTemplate.getForObject(stringBuilder.toString(), Price.class);
         } catch (Exception ex) {
             System.out.println("JEUSDEBUG: ERROR   " + ex.getMessage());
         }
         System.out.println(price);
         return price;
     }
+
+
+    private String getUrl(String host,int port) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("http://").append(host).append(":").append(port).append("/price/");
+        return stringBuilder.toString();
+    }
+
 }
