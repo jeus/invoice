01f37e866c7493cb9c366851ef4b457f662b5044
Administrator <alkhandani@gmail.com>
2018-11-12 19:34:45 +0330

    change gitlabfile and dockerfile ci44

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -24,9 +24,9 @@ SET row_security = off;
 
 DROP SCHEMA IF EXISTS old_invoice3 cascade;
 
-ALTER SCHEMA IF EXISTS name old_invoice2 TO old_invoice3
-ALTER SCHEMA IF EXISTS name old_invoice1 TO old_invoice2
-ALTER SCHEMA IF EXISTS name invoicing TO old_invoice1
+ALTER SCHEMA IF EXISTS name old_invoice2 TO old_invoice3;
+ALTER SCHEMA IF EXISTS name old_invoice1 TO old_invoice2;
+ALTER SCHEMA IF EXISTS name invoicing TO old_invoice1;
 
 CREATE SCHEMA invoicing;
 
