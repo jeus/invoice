2e771c05dbecce2052d74cd0e44c2e87ee8cb00e
Administrator <alkhandani@gmail.com>
2018-11-12 17:09:35 +0330

    change gitlabfile and dockerfile ci41

--- src/main/resources/sql/backup.sh
+++ src/main/resources/sql/backup.sh
@@ -5,4 +5,3 @@ pg_dump -h localhost -p 5432 -U jeus -n invoicing -b -v -f staging.sql
 
 #get backup by insert command (slower and larger)
 pg_dump -h localhost -p 5432 -U jeus -n invoicing --column-inserts -b -v -f staging1.sql
-
