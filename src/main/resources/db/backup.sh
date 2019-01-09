02c691f3e5a509e70384f917021791aac681ff94
Administrator <alkhandani@gmail.com>
2019-01-09 18:30:17 +0330

    add gitlab-ci ssh and run remote command refs #850

--- src/main/resources/db/backup.sh
+++ src/main/resources/db/backup.sh
@@ -5,3 +5,10 @@ pg_dump -h localhost -p 5432 -U jeus -n invoicing -b -v -f invoicing_batch.sql
 
 #get backup by insert command (slower and larger)
 pg_dump -h localhost -p 5432 -U jeus -n invoicing --column-inserts -b -v -f invoicing_insert.sql
+#!/usr/bin/env bash
+
+#get backup by copy command (fast and smal)
+pg_dump -h localhost -p 5432 -U jeus -n invoicing -b -v -f invoicing_batch.sql
+
+#get backup by insert command (slower and larger)
+pg_dump -h localhost -p 5432 -U jeus -n invoicing --column-inserts -b -v -f invoicing_insert.sql
